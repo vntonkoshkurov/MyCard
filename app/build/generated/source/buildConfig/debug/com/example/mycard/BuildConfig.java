5256eb15fa900a7c20f4b66795839054f4f093b2
Vasily <vntonkoshkurov@yandex.ru>
2023-03-15 02:33:54 +0600

    Add local files

---
+++ app/build/generated/source/buildConfig/debug/com/example/mycard/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.mycard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.mycard";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "2.0.4";
+}
